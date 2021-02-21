d25c3d77230fe960d1a6f0dd0cec41978276fc05
Demchenko.Evgeny <etaka@bk.ru>
2021-02-21 19:29:48 +0300

    Initial Commit

---
+++ parseJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parseJSON
+//
+//  Created by Евгений Демченко on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
